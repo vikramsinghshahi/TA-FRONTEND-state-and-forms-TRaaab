9fb3c8ab801d64826da5bd31bf666dbf52e213a5
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-10-28 14:54:06 +0530

    update

---
+++ block-BRaabr/code/form-app/src/components/App.js
@@ -0,0 +1,16 @@
+import MultipleInput from './Form1';
+import FormValidation from './Form2';
+import '../styles/index.css';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <MultipleInput />
+        <FormValidation />
+      </div>
+    </>
+  );
+}
+
+export default App;
